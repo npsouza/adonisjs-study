009f45ea3fd248e2c7d9b019377c90ffd8786a0c
Nathan <nathanpereirasouza@gmail.com>
2020-01-12 12:02:58 -0300

    criando estrutura pra salvar arquivos

--- start/routes.js
+++ start/routes.js
@@ -21,3 +21,6 @@ Route.post('sessions', 'SessionController.store')
 
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.post('files', 'FileController.store')
+Route.get('files/:id', 'FileController.show')
