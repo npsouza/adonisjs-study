00c650b17aa49b657f1b369701b42a677830faf3
Nathan <nathanpereirasouza@gmail.com>
2020-01-12 11:13:05 -0300

    configurando reset de senha e envio de emails

---
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const crypto = require('crypto')
+const User = use('App/Models/User')
+const Mail = use('Mail')
+const Env = use('Env')
+
+class ForgotPasswordController {
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
+
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
+
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password', 'emails.forgot_password-text'],
+        { email, token: user.token, link: `${request.input('redirect_url')}?token=${user.token}` },
+        message => {
+          message
+            .to(user.email)
+            .from(Env.get('MAIL_FROM'))
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (err) {
+      console.log(err)
+      return response.status(err.status).send({
+        error: {
+          message: 'Invalid email'
+        }
+      })
+    }
+  }
+}
+
+module.exports = ForgotPasswordController
