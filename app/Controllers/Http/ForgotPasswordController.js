4542326efc376a176546fc13e45c1b3fb274c7f1
Nathan <nathanpereirasouza@gmail.com>
2020-01-12 11:20:31 -0300

    configurando atualizar senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const User = use('App/Models/User')
 const Mail = use('Mail')
@@ -27,10 +28,41 @@ class ForgotPasswordController {
         }
       )
     } catch (err) {
-      console.log(err)
       return response.status(err.status).send({
         error: {
           message: 'Invalid email'
+        }
+      })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response.status(401).send({
+          error: {
+            message: 'Expired token'
+          }
+        })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response.status(err.status).send({
+        error: {
+          message: 'Unknown error'
         }
       })
     }
